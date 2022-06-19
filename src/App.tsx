04f878350930558017d308069236efe8405b24a0
willyoliv <willyoliversilva@gmail.com>
2022-06-18 22:26:18 -0300

    subindo a primeira versão

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskContainer from './components/TaskContainer';
+import TaskItem from './components/TaskItem';
+import "./styles/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+     <h1 className="m-5">📋 Task Manager</h1>
+     <div id="new-task">
+      <button id="btnAdd" className="btn btn-primary" >
+        add
+      </button>
+     </div>
+     <TaskContainer>
+        <TaskItem titulo="tarefa 1" />
+        <TaskItem titulo="tarefa 2" done={true} />
+     </TaskContainer>
+    </main>
   );
 }
 
